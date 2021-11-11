ee7db603252dc0992ae518c5bfefa2e20ce9f341
Gautham Nagrajan <gcn5@njit.edu>
2021-11-10 23:51:06 -0500

    trying to figure out why I can't merge

--- partials/nav.php
+++ partials/nav.php
@@ -26,7 +26,7 @@ require_once(__DIR__ . "/../lib/functions.php");
 <!-- include css and js files -->
 <link rel="stylesheet" href="<?php echo get_url('styles.css'); ?>">
 <script src="<?php echo get_url('helpers.js'); ?>"></script>
-<nav>
+<nav> 
     <ul>
         <?php if (is_logged_in()) : ?>
             <li><a href="<?php echo get_url('home.php'); ?>">Home</a></li>
